3e38065ecb3d7e29865d879e84a48fa81f3248c9
VuHaiHau <trachanhsp2@gmail.com>
2019-06-03 17:09:15 +0700

    Day code b2 len

---
+++ Hau/BaoCao.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace TTN_QLTV
+{
+    partial class BaoCao
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rpv_baocao = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.SuspendLayout();
+            // 
+            // rpv_baocao
+            // 
+            this.rpv_baocao.Location = new System.Drawing.Point(-1, 12);
+            this.rpv_baocao.Name = "rpv_baocao";
+            this.rpv_baocao.ServerReport.BearerToken = null;
+            this.rpv_baocao.Size = new System.Drawing.Size(720, 642);
+            this.rpv_baocao.TabIndex = 0;
+            this.rpv_baocao.Load += new System.EventHandler(this.reportViewer1_Load);
+            // 
+            // BaoCao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 655);
+            this.Controls.Add(this.rpv_baocao);
+            this.Name = "BaoCao";
+            this.Text = "BaoCao";
+            this.Load += new System.EventHandler(this.BaoCao_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer rpv_baocao;
+    }
+}
---
+++ Hau/BaoCao.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Microsoft.Reporting.WinForms;
+
+namespace TTN_QLTV
+{
+    public partial class BaoCao : Form
+    {
+        accessdata acc = new accessdata();
+        public static int keyn = 0;
+        public static string MaPM;
+        public BaoCao()
+        {
+            InitializeComponent();
+        }
+
+        private void BaoCao_Load(object sender, EventArgs e)
+        {
+            if (keyn == 1)
+            {
+
+                SqlConnection con = new SqlConnection();
+                string sql = @"Data Source=DUONG-PC\SQLEXPRESS;Initial Catalog=QLTV;Integrated Security=True";
+                con.ConnectionString = sql;
+                con.Open();
+                acc.OpenConnect();
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "INRAPHIEUMUON";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add(new SqlParameter("@mapm", MaPM));
+                cmd.Connection = con;
+                cmd.ExecuteNonQuery();
+                DataSet ds = new DataSet();
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                sda.Fill(ds);
+                rpv_baocao.ProcessingMode = ProcessingMode.Local;
+                rpv_baocao.LocalReport.ReportPath = "XuatPhieuMuon.rdlc";
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    ReportDataSource rds = new ReportDataSource();
+                    rds.Name = "DataSet1";
+                    rds.Value = ds.Tables[0];
+                    rpv_baocao.LocalReport.DataSources.Clear();
+                    rpv_baocao.LocalReport.DataSources.Add(rds);
+                    rpv_baocao.RefreshReport();
+                }
+                else
+                {
+                    MessageBox.Show("Không Có Dữ Liệu!");
+                    BaoCao bcf = new BaoCao();
+                    bcf.Hide();
+                }
+            }
+            if (keyn == 2)
+            {
+
+                SqlConnection con = new SqlConnection();
+                string sql = @"Data Source=DUONG-PC\SQLEXPRESS;Initial Catalog=QLTV;Integrated Security=True";
+                con.ConnectionString = sql;
+                con.Open();
+                acc.OpenConnect();
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "INRAPHIEUTRA";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add(new SqlParameter("@mapm", MaPM));
+                cmd.Connection = con;
+                cmd.ExecuteNonQuery();
+                DataSet ds = new DataSet();
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                sda.Fill(ds);
+                rpv_baocao.ProcessingMode = ProcessingMode.Local;
+                rpv_baocao.LocalReport.ReportPath = "XuatPhieuTra.rdlc";
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    ReportDataSource rds = new ReportDataSource();
+                    rds.Name = "DataSet1";
+                    rds.Value = ds.Tables[0];
+                    rpv_baocao.LocalReport.DataSources.Clear();
+                    rpv_baocao.LocalReport.DataSources.Add(rds);
+                    rpv_baocao.RefreshReport();
+                }
+                else
+                {
+                    MessageBox.Show("Không Có Dữ Liệu!");
+                    BaoCao bcf = new BaoCao();
+                    bcf.Hide();
+                }
+            }
+
+        }
+
+        private void reportViewer1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
